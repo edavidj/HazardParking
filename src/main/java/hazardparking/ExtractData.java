bd1215aa1eace57b5bb1c0764fb24da2f4fb2ab3
Rico Florentino <florente@mcmaster.ca>
2018-03-27 22:30:53 -0400

    Changed data structure, finalized sorting

--- src/main/java/hazardparking/ExtractData.java
+++ src/main/java/hazardparking/ExtractData.java
@@ -12,23 +12,25 @@ import java.util.ArrayList;
  * @author Steven Luu, Rico Florentino & Ethan Johnston
  */
 public class ExtractData  {
-	private static ArrayList<Entry> data;
+	private static Entry[] data;
 	public static void init() throws Exception{
         data = extract();
     }
-    private static ArrayList<Entry> extract() throws Exception{
+    private static Entry[] extract() throws Exception{
         //Define buffered reader for reading file
         File file = new ClassPathResource("data/parking.csv").getFile();
         BufferedReader br = new BufferedReader(new FileReader(file));
 
         br.readLine();
 
-        ArrayList<Entry> entries = new ArrayList<Entry>();
+        Entry[] entries = new Entry[132850];
 
         String entryinfo = br.readLine();
         LocalDateTime tempdate;
 
+        int i = 0;
         while (entryinfo != null) {
+
             String[] splitinfo = entryinfo.split(",");
 
             if (splitinfo[8].length() == 0) {
@@ -45,20 +47,21 @@ public class ExtractData  {
                 tempdate = LocalDateTime.of(Integer.parseInt(splitinfo[18].substring(0, 4)),Integer.parseInt(splitinfo[18].substring(5, 7)), Integer.parseInt(splitinfo[18].substring(8, 10)),00,Integer.parseInt(splitinfo[8]));
             }
 
-            entries.add(new Entry(Float.parseFloat((splitinfo[0])),Float.parseFloat(splitinfo[1]),splitinfo[4],tempdate,splitinfo[9],splitinfo[10],splitinfo[11]));
+            entries[i]= new Entry(Float.parseFloat((splitinfo[0])),Float.parseFloat(splitinfo[1]),splitinfo[4],tempdate,splitinfo[9],splitinfo[10],splitinfo[11]);
+            i++;
             entryinfo = br.readLine();
 
         }
         return entries;
     }
 
-    public static ArrayList<Entry> getData() {
+    public static Entry[] getData() {
         return data;
     }
     public static Entry getIndex(int index){
-	    return data.get(index);
+	    return data[index];
     }
-    public static void setData(ArrayList<Entry> newData) {
+    public static void setData(Entry[] newData) {
         data = newData;
     }
 }
