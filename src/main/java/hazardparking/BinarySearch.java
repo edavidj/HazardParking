a9e1019eb5ca456c31f7b3244a5c6f87c93b1ed3
Steven Luu <luus1@mcmaster.ca>
2018-03-29 17:25:26 -0400

    Filter functionality added

---
+++ src/main/java/hazardparking/BinarySearch.java
@@ -0,0 +1,107 @@
+/**
+    Author- Steven Luu
+    This class uses binary search to search through the Entry array looking for either a specific day, violation code, date, or hour
+    When a search is found, it will return the index of the matched Entry.
+ */
+package hazardparking;
+
+import java.time.LocalDateTime;
+
+public class BinarySearch {
+    private static int mid = 0;
+
+    /**
+        Option 1- Day of the Week
+        Option 2- Violation Code
+     */
+    public static int search(Entry[] data, int min, int max, String target, int option){
+        if (max >= min) {
+            mid = min + (max - min) / 2;
+            if (option == 1){
+                if (compare(target, data[mid].getDay()) == 0)
+                    return mid;
+                else if (compare(target, data[mid].getDay()) > 0)
+                    return search(data, mid + 1, max, target, option);
+                else
+                    return search(data, min, mid - 1, target, option);
+            }
+            else{
+                if (target.equals(data[mid].getViolationCode())){
+                    return mid;
+                }
+                else if (target.compareTo(data[mid].getViolationCode()) > 0){
+                    return search(data, mid + 1, max, target, option);
+                }
+                else{
+                    return search(data, min, mid - 1, target, option);
+                }
+            }
+        }
+        return -1;
+    }
+
+    /**
+        Option 1- Local Date
+        Option 2- Hour
+     */
+    public static int search(Entry[] data, int min, int max, LocalDateTime target, int option){
+        if (max >= min) {
+            mid = min + (max - min) / 2;
+            if (option == 1){
+                if (target.getDayOfMonth() == data[mid].getDate().getDayOfMonth()){
+                    return mid;
+                }
+                else if (target.getDayOfMonth() > data[mid].getDate().getDayOfMonth()){
+                    return search(data, mid + 1, max, target, option);
+                }
+                else{
+                    return search(data, min, mid - 1, target, option);
+                }
+            }
+            else{
+                if (target.getHour() == data[mid].getDate().getHour()){
+                    return mid;
+                }
+                else if (target.getHour() > data[mid].getDate().getHour()){
+                    return search(data, mid + 1, max, target, option);
+                }
+                else{
+                    return search(data, min, mid - 1, target, option);
+                }
+
+            }
+        }
+        return -1;
+    }
+
+    private static int compare(String s1, String s2){
+        if (convert(s1) == convert(s2))
+            return 0;
+        else if (convert(s1) > convert(s2))
+            return 1;
+        else
+            return -1;
+    }
+
+    private static int convert(String s){
+
+        switch (s){
+            case "MONDAY":
+                return 1;
+            case "TUESDAY":
+                return 2;
+            case "WEDNESDAY":
+                return 3;
+            case "THURSDAY":
+                return 4;
+            case "FRIDAY":
+                return 5;
+            case "SATURDAY":
+                return 6;
+            case "SUNDAY":
+                return 7;
+            default:
+                return -1;
+        }
+    }
+}
---
+++ src/main/java/hazardparking/Filter.java
@@ -0,0 +1,127 @@
+/**
+    Author- Steven Luu
+    This class uses binary search to locate a match in the Entry array, it then finds all other entries that have the same match
+    returning a subarray of a specified filter
+    Possible inputs for filter parameter:
+    day- "MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY", "SUNDAY"
+    violationCode- "Pxxx" where x is a natural number (include leading 0's if the code is less than 100)
+    date- Any whole number from 1-31
+    hour- Any whole number from 0-23 where 0 is midnight (12AM)
+ */
+package hazardparking;
+
+import org.apache.tomcat.jni.Local;
+
+import java.time.LocalDateTime;
+import java.util.Arrays;
+
+public class Filter {
+    private static int top;
+    private static int bottom;
+    private static boolean floor = false;
+    private static boolean ceiling = false;
+
+    public static Entry[] day(Entry[] data, String filter){
+        Sort.sort(data, 4);
+        top = BinarySearch.search(data, 0, 132850, filter, 1);
+        bottom = top;
+        while (!ceiling || !floor){
+            if (!ceiling){
+                if (top == 132850)
+                    ceiling = true;
+                else if (!data[top+1].getDay().equals(filter))
+                    ceiling = true;
+                else
+                    top++;
+            }
+            if (!floor){
+                if (bottom == 0)
+                    floor = true;
+                else if (!data[bottom-1].getDay().equals(filter))
+                    floor = true;
+                else
+                    bottom--;
+            }
+        }
+        return Arrays.copyOfRange(data, bottom, top);
+    }
+
+    public static Entry[] violationCode(Entry[] data, String filter){
+        Sort.sort(data, 3);
+        top = BinarySearch.search(data, 0, 132850, filter, 2);
+        bottom = top;
+        while (!ceiling || !floor){
+            if (!ceiling){
+                if (top == 132850)
+                    ceiling = true;
+                else if (!data[top+1].getViolationCode().equals(filter))
+                    ceiling = true;
+                else
+                    top++;
+            }
+            if (!floor){
+                if (bottom == 0)
+                    floor = true;
+                else if (!data[bottom-1].getViolationCode().equals(filter))
+                    floor = true;
+                else
+                    bottom--;
+            }
+        }
+        return Arrays.copyOfRange(data, bottom, top);
+
+    }
+
+    public static Entry[] date(Entry[] data, int filter){
+        LocalDateTime temp = LocalDateTime.of(2015,12,filter,0,0);
+        Sort.sort(data, 1);
+        top = BinarySearch.search(data, 0, 132850,temp,1);
+        bottom = top;
+        while (!ceiling || !floor){
+            if (!ceiling){
+                if (top == 132850)
+                    ceiling = true;
+                else if (data[top+1].getDate().getDayOfMonth() != filter)
+                    ceiling = true;
+                else
+                    top++;
+            }
+            if (!floor){
+                if (bottom == 0)
+                    floor = true;
+                else if (data[bottom-1].getDate().getDayOfMonth() != filter)
+                    floor = true;
+                else
+                    bottom--;
+            }
+        }
+        return Arrays.copyOfRange(data, bottom, top);
+    }
+
+    public static Entry[] hour(Entry[] data, int filter){
+        LocalDateTime temp = LocalDateTime.of(2015,12,1,filter,0);
+        Sort.sort(data, 2);
+        top = BinarySearch.search(data, 0, 132850, temp, 2);
+        bottom = top;
+        while (!ceiling || !floor){
+            if (!ceiling){
+                if (top == 132850)
+                    ceiling = true;
+                else if (data[top+1].getDate().getHour() != filter)
+                    ceiling = true;
+                else
+                    top++;
+            }
+            if (!floor){
+                if (bottom == 0)
+                    floor = true;
+                else if (data[bottom-1].getDate().getHour() != filter)
+                    floor = true;
+                else
+                    bottom--;
+            }
+        }
+        return Arrays.copyOfRange(data, bottom, top);
+    }
+
+}
